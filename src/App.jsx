ad2f3eea51950ab3fbcb9ce824c9f9835a271ccb
Aayush Trivedi <aayush.t200@gmail.com>
2026-05-14 12:28:44 +0530

    feat: reset to initial project structure with HTML, CSS, and React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState, useEffect } from 'react';
-import getData from './services/api.js';
 import './App.css';
+
 export default function App() {
-    let [newData, setData] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const data = await getData();
-            setData(data);
-        }
-        fetchData();
-    }, []);
-    console.log(newData);
     return (
-        <>
-            <h1>Hello world!</h1>
-        </>
+        <div className="app">
+            <h1>Start building here</h1>
+        </div>
     );
 }
